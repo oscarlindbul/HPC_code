dc3cf05b4994d4e8590224282fed31c5578ae538
Yu Yang <yuyang2@kth.se>
2020-10-13 10:16:42 +0200

    organize folders

--- ass3/yu/ex2/Tree_Blocking/compile.sh
+++
@@ -1,4 +0,0 @@
-files=(*.c)
-for file in ${files[@]}; do
-	cc $file -o ${file%.c}.out
-done
--- ass3/yu/ex2/Tree_Blocking/run.sh
+++
@@ -1,6 +0,0 @@
-procs=(8 16 32 64 128)
-
-for proc in ${procs[@]}; do
-	sed "s/processes/${proc}/g" batch_run.sh > tmp.sh
-	sbatch tmp.sh
-done
